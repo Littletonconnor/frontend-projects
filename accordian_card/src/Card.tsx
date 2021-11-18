88f538d97510cafe985234e4b3d27ea859ba4c03
connorlittleton <connorlittleton@wealthfront.com>
2021-11-17 20:50:05 -0800

    Add image and make mobile friendly

--- accordian_card/src/Card.tsx
+++ accordian_card/src/Card.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Accordian from "./Accordian";
+import img from "./images/illustration-woman-online-desktop.svg";
+import box from "./images/illustration-box-desktop.svg";
+import shadow from "./images/bg-pattern-desktop.svg";
 
 function Card() {
   return (
     <div className="container">
       <div className="card-container">
-        <div>Image Stuff</div>
+        <div className="image-container">
+          <img className="img lady-img" src={img} alt="meta" />
+          <img className="img shadow-img" src={shadow} alt="meta" />
+          <img className="img box-img" src={box} alt="meta" />
+        </div>
         <div className="faq">
           <h1>faq</h1>
           <Accordian />
